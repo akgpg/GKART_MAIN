c12c4a932bc71c3743de1fce175fb91da300cb72
Amit <akgpg@users.noreply.github.com>
2016-12-07 00:06:36 +0530

    Initial Commit

---
+++ Sequence/gkt_ads_seq.sql
@@ -0,0 +1,8 @@
+/*
+Description : This sequence will generate the id for the newly inserted item  into  gkt_adress table.
+
+Name       Date        Description
+---------- ----------  ------------------------------------------------
+Amit Giri  22-09-2016  Initial creation
+****************************************************************************************************************/
+create sequence gkt_ads_seq start with 1 increment by 1;
